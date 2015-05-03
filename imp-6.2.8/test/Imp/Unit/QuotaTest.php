fa0084dcc5ace29a8fcb849aecc03a9e61190f80
Mathieu Parent <math.parent@gmail.com>
2015-05-03 22:55:34 +0200

    Imported Upstream version 6.2.8

---
+++ imp-6.2.8/test/Imp/Unit/QuotaTest.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Copyright 2010-2015 Horde LLC (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (GPL). If you
+ * did not receive this file, see http://www.horde.org/licenses/gpl.
+ *
+ * @category   Horde
+ * @copyright 2010-2015 Horde LLC
+ * @license    http://www.horde.org/licenses/gpl GPL
+ * @package    IMP
+ * @subpackage UnitTests
+ */
+
+/**
+ * Test the Quota library.
+ *
+ * @author     Michael Slusarz <slusarz@horde.org>
+ * @category   Horde
+ * @copyright  2010-2015 Horde LLC
+ * @ignore
+ * @license    http://www.horde.org/licenses/gpl GPL
+ * @package    IMP
+ * @subpackage UnitTests
+ */
+class Imp_Unit_QuotaTest extends PHPUnit_Framework_TestCase
+{
+    public function testMaildir()
+    {
+        $quota = new IMP_Quota_Null();
+        $data = $quota->getQuota();
+
+        $this->assertEquals(
+            0,
+            $data['limit']
+        );
+
+        $this->assertEquals(
+            0,
+            $data['usage']
+        );
+    }
+
+}
